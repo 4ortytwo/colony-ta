aba37696f4fc422e6318a97b7231a0dff21e0b1d
4ortytwo <arthur.burgan@spronq.com>
2022-05-20 07:10:39 +0200

    complete TA

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import EventsList from './components/EventsList';
+
+function App() {
+  return (
+    <div className="App">
+      <EventsList />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Config, DAppProvider, Mainnet } from '@usedapp/core';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const REACT_APP_PROVIDER = process.env?.REACT_APP_PROVIDER ?? '';
+
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: REACT_APP_PROVIDER,
+  },
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
